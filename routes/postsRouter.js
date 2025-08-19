834dae97b75779d1bdaab4f77e9c5a96f5c2bb92
Adrian-Koh <adriankoh216@yahoo.com>
2025-08-19 19:18:29 +0800

    Extract form field values in POST for posts

--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -14,27 +14,46 @@ postsRouter.get("/:postId", (req, res, next) => {
   res.send("GET post id " + postId);
 });
 
-postsRouter.post("/:postId", verifyToken, (req, res, next) => {
+postsRouter.post("/", verifyToken, (req, res, next) => {
+  jwt.verify(req.token, process.env.SECRET_KEY, (err, authData) => {
+    if (err) {
+      next(err);
+    } else {
+      const userId = authData.user.id;
+      const { title, text, publish } = req.body;
+      const addedTime = new Date();
+      res.json({
+        message: `POST from user ${userId}, title: ${title}, text: ${text}, addedTime: ${addedTime.toLocaleDateString()}, ${addedTime.toLocaleTimeString()}, publish: ${publish}`,
+      });
+      // TODO: add to prisma
+    }
+  });
+});
+
+postsRouter.put("/:postId", verifyToken, (req, res, next) => {
   jwt.verify(req.token, process.env.SECRET_KEY, (err, authData) => {
     if (err) {
       next(err);
     } else {
       const { postId } = req.params;
       res.json({
-        message: "POST post id " + postId,
+        message: "PUT post id " + postId,
       });
     }
   });
 });
 
-postsRouter.put("/:postId", (req, res, next) => {
-  const { postId } = req.params;
-  res.send("PUT post id " + postId);
-});
-
-postsRouter.delete("/:postId", (req, res, next) => {
-  const { postId } = req.params;
-  res.send("DELETE post id " + postId);
+postsRouter.delete("/:postId", verifyToken, (req, res, next) => {
+  jwt.verify(req.token, process.env.SECRET_KEY, (err, authData) => {
+    if (err) {
+      next(err);
+    } else {
+      const { postId } = req.params;
+      res.json({
+        message: "DELETE post id " + postId,
+      });
+    }
+  });
 });
 
 module.exports = postsRouter;
